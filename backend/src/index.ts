1f6284708751761fe9b4c248474ed231866dda21
lysy-dev <lysik.karol39@gmail.com>
2023-02-19 16:20:03 +0100

    Add DB access to KOA

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,13 @@
 import koa from "koa";
 import { enableMethods } from "./router/index.js";
-// import { DB } from "./db";
+import { DB } from "./db/index.js";
 
-// const router = new Router();
-// router.get("/test", async (ctx) => {
-//   const { db } = ctx;
-
-//   const user = db.user.get("1");
-//   ctx.body = user;
-// });
 
 const app = new koa();
 
+app.context.db = DB.getInstance();
 app.use(enableMethods)
-app.use(ctx=>{ctx.body = ctx})
-// app.context.db = DB.getInstance();
+app.use(ctx=>{ctx.body = ctx.db})
+
 app.listen(3005);
 console.log("started on port 3005");
--- backend/src/router/index.ts
+++ backend/src/router/index.ts
@@ -3,7 +3,6 @@ import { Context, Next } from "koa";
 const availableMethods = [ "GET", "POST", "PUT", "DELETE"];
 
 export const enableMethods = (ctx:Context, next:Next)=>{
-    console.log(ctx.request.method)
     if(!availableMethods.includes(ctx.request.method)) {
         return ctx.throw(403, "Method not allowed");
     }
