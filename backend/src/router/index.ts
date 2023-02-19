f9c9bb2132e2737755637f3f5b60249b7eca0a15
lysy-dev <lysik.karol39@gmail.com>
2023-02-19 21:36:20 +0100

    Add rest getter

--- backend/src/router/index.ts
+++ backend/src/router/index.ts
@@ -1,10 +1,14 @@
 import { Context, Next } from "koa";
 
-const availableMethods = [ "GET", "POST", "PUT", "DELETE"];
+const availableMethods = ["GET"];
 
-export const enableMethods = (ctx:Context, next:Next)=>{
-    if(!availableMethods.includes(ctx.request.method)) {
-        return ctx.throw(403, "Method not allowed");
-    }
-    return next();
+export const enableMethods = (ctx: Context, next: Next) => {
+  if (!availableMethods.includes(ctx.request.method)) {
+    return ctx.throw(403, "Method not allowed");
+  }
+  return next();
+};
+
+export const routeHandler = (ctx: Context, next: Next) => {
+  return next();
 }
