db2af24755bdf98bf3fccf6cc498bcbe90ba9ada
lysy-dev <lysik.karol39@gmail.com>
2023-02-21 20:49:56 +0100

    create form

--- frontend/src/ContactsPage/components/ContactsList/ContactCard/InsertContactCard.tsx
+++ frontend/src/ContactsPage/components/ContactsList/ContactCard/InsertContactCard.tsx
@@ -1,22 +1,22 @@
+import { useForm } from "react-hook-form";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import AddIcon from "@mui/icons-material/Add";
-import { AddContact } from "../../../types";
+import { AddContact, NewContact } from "../../../types";
+import { FormControl, FormHelperText, Input, InputLabel } from "@mui/material";
+// addContact({
+//   email: "test@test.pl",
+//   first_name: "test",
+//   last_name: "test",
+//   avatar: "test",
+//   gender: "unknown",
+// });
 
 const _AddContactCard = ({ addContact }: { addContact: AddContact }) => {
-  
   return (
     <ListItem>
       <ListItemButton
-        onClick={(_) => {
-          addContact({
-            email: "test@test.pl",
-            first_name: "test",
-            last_name: "test",
-            avatar: "test",
-            gender: "unknown",
-          });
-        }}
+       
         dense
       >
         <AddIcon />
@@ -26,3 +26,22 @@ const _AddContactCard = ({ addContact }: { addContact: AddContact }) => {
 };
 
 export const AddContactCard = _AddContactCard;
+
+const FormCard = () => {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm<NewContact>();
+  return (
+    <FormControl>
+      <InputLabel htmlFor="email">Email address</InputLabel>
+      <Input id="my-input" aria-describedby="my-helper-text" {...register("email",{required:true})}/>
+
+      <InputLabel htmlFor="first_name">First name</InputLabel>
+      <Input id="first_name" aria-describedby="my-helper-text" {...register("first_name",{required:true})}/>
+
+      <InputLabel htmlFor="last_name">Last name</InputLabel>
+      <Input id="last_name" aria-describedby="my-helper-text" {...register("last_name",{required:true})}/>
+      <FormHelperText id="my-helper-text">
+        We'll never share your email.
+      </FormHelperText>
+    </FormControl>
+  );
+};
--- frontend/src/ContactsPage/types.ts
+++ frontend/src/ContactsPage/types.ts
@@ -2,9 +2,9 @@ export type Contact = {
   id: number;
   first_name: string;
   last_name: string;
-  email: string;
-  gender: string;
-  avatar: string;
+  email?: string;
+  gender?: string;
+  avatar?: string;
 };
 
 export type ContactsList = Array<Contact>;
